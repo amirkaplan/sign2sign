aefc666d55a24154f92d7ad43090f18090e9936d
צחי בן חמו <tzachi222@Gmail.com>
2018-10-24 18:13:30 +0300

    style changed

--- sign2sign.player/src/pages/home/home.ts
+++ sign2sign.player/src/pages/home/home.ts
@@ -7,7 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+   windows = [{name:"win1", x:0, y:0, width:"30" ,height:"20", style:''},{name:"win2", x:500, y:0, width:"40" ,height:"50", style:''}]
+  
+
   constructor(public navCtrl: NavController) {
+
+    this.windows.forEach(e => {
+      e.style = `width: ${e.width}px; height:100px`;
+      e.width = e.width + "%";
+      e.height = e.height + "%";
+    
+    });
 
   }
 
